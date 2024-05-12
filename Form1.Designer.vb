735cb5e5530f8528cf3cb85ec877b6e113110cdc
CHEMI <107860482+shMetrochemi@users.noreply.github.com>
2024-05-12 11:19:19 +0800

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,385 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+	Inherits System.Windows.Forms.Form
+
+	'Form 重写 Dispose，以清理组件列表。
+	<System.Diagnostics.DebuggerNonUserCode()> _
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+	'Windows 窗体设计器所必需的
+	Private components As System.ComponentModel.IContainer
+
+	'注意: 以下过程是 Windows 窗体设计器所必需的
+	'可以使用 Windows 窗体设计器修改它。  
+	'不要使用代码编辑器修改它。
+	<System.Diagnostics.DebuggerStepThrough()> _
+	Private Sub InitializeComponent()
+		Me.components = New System.ComponentModel.Container()
+		Me.Label1 = New System.Windows.Forms.Label()
+		Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+		Me.关于ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+		Me.新项目ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+		Me.Button1 = New System.Windows.Forms.Button()
+		Me.Button2 = New System.Windows.Forms.Button()
+		Me.Button3 = New System.Windows.Forms.Button()
+		Me.Button4 = New System.Windows.Forms.Button()
+		Me.Label2 = New System.Windows.Forms.Label()
+		Me.TextBox1 = New System.Windows.Forms.TextBox()
+		Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+		Me.Label3 = New System.Windows.Forms.Label()
+		Me.Label4 = New System.Windows.Forms.Label()
+		Me.Label5 = New System.Windows.Forms.Label()
+		Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+		Me.TextBox3 = New System.Windows.Forms.TextBox()
+		Me.Label7 = New System.Windows.Forms.Label()
+		Me.TextBox2 = New System.Windows.Forms.TextBox()
+		Me.Label6 = New System.Windows.Forms.Label()
+		Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+		Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+		Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+		Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+		Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+		Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+		Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+		Me.Label8 = New System.Windows.Forms.Label()
+		Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+		Me.MenuStrip1.SuspendLayout()
+		Me.GroupBox1.SuspendLayout()
+		Me.GroupBox2.SuspendLayout()
+		Me.GroupBox3.SuspendLayout()
+		Me.SuspendLayout()
+		'
+		'Label1
+		'
+		Me.Label1.AutoSize = True
+		Me.Label1.BackColor = System.Drawing.SystemColors.ActiveCaption
+		Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		Me.Label1.Font = New System.Drawing.Font("宋体", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+		Me.Label1.Location = New System.Drawing.Point(0, 25)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(96, 458)
+		Me.Label1.TabIndex = 0
+		Me.Label1.Text = "       " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+		'
+		'MenuStrip1
+		'
+		Me.MenuStrip1.BackColor = System.Drawing.SystemColors.ActiveCaption
+		Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.关于ToolStripMenuItem, Me.新项目ToolStripMenuItem})
+		Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+		Me.MenuStrip1.Name = "MenuStrip1"
+		Me.MenuStrip1.Size = New System.Drawing.Size(800, 25)
+		Me.MenuStrip1.TabIndex = 1
+		Me.MenuStrip1.Text = "MenuStrip1"
+		'
+		'关于ToolStripMenuItem
+		'
+		Me.关于ToolStripMenuItem.Name = "关于ToolStripMenuItem"
+		Me.关于ToolStripMenuItem.Size = New System.Drawing.Size(44, 21)
+		Me.关于ToolStripMenuItem.Text = "关于"
+		'
+		'新项目ToolStripMenuItem
+		'
+		Me.新项目ToolStripMenuItem.Name = "新项目ToolStripMenuItem"
+		Me.新项目ToolStripMenuItem.Size = New System.Drawing.Size(56, 21)
+		Me.新项目ToolStripMenuItem.Text = "新项目"
+		'
+		'Button1
+		'
+		Me.Button1.Location = New System.Drawing.Point(12, 145)
+		Me.Button1.Name = "Button1"
+		Me.Button1.Size = New System.Drawing.Size(75, 23)
+		Me.Button1.TabIndex = 2
+		Me.Button1.Text = "生成代码"
+		Me.Button1.UseVisualStyleBackColor = True
+		'
+		'Button2
+		'
+		Me.Button2.Location = New System.Drawing.Point(12, 174)
+		Me.Button2.Name = "Button2"
+		Me.Button2.Size = New System.Drawing.Size(75, 23)
+		Me.Button2.TabIndex = 3
+		Me.Button2.Text = "清空预设"
+		Me.Button2.UseVisualStyleBackColor = True
+		'
+		'Button3
+		'
+		Me.Button3.Location = New System.Drawing.Point(12, 203)
+		Me.Button3.Name = "Button3"
+		Me.Button3.Size = New System.Drawing.Size(75, 37)
+		Me.Button3.TabIndex = 4
+		Me.Button3.Text = "输出Helloworld"
+		Me.Button3.UseVisualStyleBackColor = True
+		'
+		'Button4
+		'
+		Me.Button4.Location = New System.Drawing.Point(12, 246)
+		Me.Button4.Name = "Button4"
+		Me.Button4.Size = New System.Drawing.Size(75, 23)
+		Me.Button4.TabIndex = 5
+		Me.Button4.Text = "清空代码"
+		Me.Button4.UseVisualStyleBackColor = True
+		'
+		'Label2
+		'
+		Me.Label2.AutoSize = True
+		Me.Label2.BackColor = System.Drawing.SystemColors.ActiveCaption
+		Me.Label2.Location = New System.Drawing.Point(34, 119)
+		Me.Label2.Name = "Label2"
+		Me.Label2.Size = New System.Drawing.Size(29, 12)
+		Me.Label2.TabIndex = 6
+		Me.Label2.Text = "行为"
+		'
+		'TextBox1
+		'
+		Me.TextBox1.BackColor = System.Drawing.SystemColors.Window
+		Me.TextBox1.Location = New System.Drawing.Point(102, 203)
+		Me.TextBox1.Multiline = True
+		Me.TextBox1.Name = "TextBox1"
+		Me.TextBox1.Size = New System.Drawing.Size(686, 172)
+		Me.TextBox1.TabIndex = 7
+		'
+		'ProgressBar1
+		'
+		Me.ProgressBar1.Location = New System.Drawing.Point(102, 381)
+		Me.ProgressBar1.Name = "ProgressBar1"
+		Me.ProgressBar1.Size = New System.Drawing.Size(593, 31)
+		Me.ProgressBar1.TabIndex = 8
+		'
+		'Label3
+		'
+		Me.Label3.AutoSize = True
+		Me.Label3.Location = New System.Drawing.Point(701, 391)
+		Me.Label3.Name = "Label3"
+		Me.Label3.Size = New System.Drawing.Size(65, 12)
+		Me.Label3.TabIndex = 9
+		Me.Label3.Text = "检查进度："
+		'
+		'Label4
+		'
+		Me.Label4.AutoSize = True
+		Me.Label4.Location = New System.Drawing.Point(760, 391)
+		Me.Label4.Name = "Label4"
+		Me.Label4.Size = New System.Drawing.Size(11, 12)
+		Me.Label4.TabIndex = 10
+		Me.Label4.Text = "0"
+		'
+		'Label5
+		'
+		Me.Label5.AutoSize = True
+		Me.Label5.Location = New System.Drawing.Point(777, 391)
+		Me.Label5.Name = "Label5"
+		Me.Label5.Size = New System.Drawing.Size(11, 12)
+		Me.Label5.TabIndex = 11
+		Me.Label5.Text = "%"
+		'
+		'GroupBox1
+		'
+		Me.GroupBox1.Controls.Add(Me.TextBox3)
+		Me.GroupBox1.Controls.Add(Me.Label7)
+		Me.GroupBox1.Controls.Add(Me.TextBox2)
+		Me.GroupBox1.Controls.Add(Me.Label6)
+		Me.GroupBox1.Location = New System.Drawing.Point(103, 29)
+		Me.GroupBox1.Name = "GroupBox1"
+		Me.GroupBox1.Size = New System.Drawing.Size(336, 139)
+		Me.GroupBox1.TabIndex = 12
+		Me.GroupBox1.TabStop = False
+		Me.GroupBox1.Text = "文字设计"
+		'
+		'TextBox3
+		'
+		Me.TextBox3.Location = New System.Drawing.Point(49, 31)
+		Me.TextBox3.Multiline = True
+		Me.TextBox3.Name = "TextBox3"
+		Me.TextBox3.Size = New System.Drawing.Size(268, 54)
+		Me.TextBox3.TabIndex = 3
+		'
+		'Label7
+		'
+		Me.Label7.AutoSize = True
+		Me.Label7.Location = New System.Drawing.Point(13, 31)
+		Me.Label7.Name = "Label7"
+		Me.Label7.Size = New System.Drawing.Size(29, 12)
+		Me.Label7.TabIndex = 2
+		Me.Label7.Text = "正文"
+		'
+		'TextBox2
+		'
+		Me.TextBox2.Location = New System.Drawing.Point(101, 101)
+		Me.TextBox2.Name = "TextBox2"
+		Me.TextBox2.Size = New System.Drawing.Size(122, 21)
+		Me.TextBox2.TabIndex = 1
+		'
+		'Label6
+		'
+		Me.Label6.AutoSize = True
+		Me.Label6.Location = New System.Drawing.Point(12, 104)
+		Me.Label6.Name = "Label6"
+		Me.Label6.Size = New System.Drawing.Size(83, 12)
+		Me.Label6.TabIndex = 0
+		Me.Label6.Text = "Title（标题）"
+		'
+		'GroupBox2
+		'
+		Me.GroupBox2.Controls.Add(Me.CheckBox3)
+		Me.GroupBox2.Controls.Add(Me.CheckBox2)
+		Me.GroupBox2.Controls.Add(Me.CheckBox1)
+		Me.GroupBox2.Location = New System.Drawing.Point(446, 28)
+		Me.GroupBox2.Name = "GroupBox2"
+		Me.GroupBox2.Size = New System.Drawing.Size(113, 140)
+		Me.GroupBox2.TabIndex = 13
+		Me.GroupBox2.TabStop = False
+		Me.GroupBox2.Text = "按钮设计"
+		'
+		'CheckBox3
+		'
+		Me.CheckBox3.AutoSize = True
+		Me.CheckBox3.Location = New System.Drawing.Point(18, 82)
+		Me.CheckBox3.Name = "CheckBox3"
+		Me.CheckBox3.Size = New System.Drawing.Size(84, 16)
+		Me.CheckBox3.TabIndex = 2
+		Me.CheckBox3.Text = "取消CANCLE"
+		Me.CheckBox3.UseVisualStyleBackColor = True
+		'
+		'CheckBox2
+		'
+		Me.CheckBox2.AutoSize = True
+		Me.CheckBox2.Location = New System.Drawing.Point(18, 60)
+		Me.CheckBox2.Name = "CheckBox2"
+		Me.CheckBox2.Size = New System.Drawing.Size(60, 16)
+		Me.CheckBox2.TabIndex = 1
+		Me.CheckBox2.Text = "否定NO"
+		Me.CheckBox2.UseVisualStyleBackColor = True
+		'
+		'CheckBox1
+		'
+		Me.CheckBox1.AutoSize = True
+		Me.CheckBox1.Location = New System.Drawing.Point(18, 37)
+		Me.CheckBox1.Name = "CheckBox1"
+		Me.CheckBox1.Size = New System.Drawing.Size(66, 16)
+		Me.CheckBox1.TabIndex = 0
+		Me.CheckBox1.Text = "确定YES"
+		Me.CheckBox1.UseVisualStyleBackColor = True
+		'
+		'GroupBox3
+		'
+		Me.GroupBox3.Controls.Add(Me.RadioButton2)
+		Me.GroupBox3.Controls.Add(Me.RadioButton1)
+		Me.GroupBox3.Location = New System.Drawing.Point(566, 29)
+		Me.GroupBox3.Name = "GroupBox3"
+		Me.GroupBox3.Size = New System.Drawing.Size(222, 139)
+		Me.GroupBox3.TabIndex = 14
+		Me.GroupBox3.TabStop = False
+		Me.GroupBox3.Text = "循环次数设定"
+		'
+		'RadioButton2
+		'
+		Me.RadioButton2.AutoSize = True
+		Me.RadioButton2.Location = New System.Drawing.Point(21, 69)
+		Me.RadioButton2.Name = "RadioButton2"
+		Me.RadioButton2.Size = New System.Drawing.Size(179, 16)
+		Me.RadioButton2.TabIndex = 17
+		Me.RadioButton2.TabStop = True
+		Me.RadioButton2.Text = "运行一次（执行结束就关闭）"
+		Me.RadioButton2.UseVisualStyleBackColor = True
+		'
+		'RadioButton1
+		'
+		Me.RadioButton1.AutoSize = True
+		Me.RadioButton1.Location = New System.Drawing.Point(21, 46)
+		Me.RadioButton1.Name = "RadioButton1"
+		Me.RadioButton1.Size = New System.Drawing.Size(143, 16)
+		Me.RadioButton1.TabIndex = 16
+		Me.RadioButton1.TabStop = True
+		Me.RadioButton1.Text = "循环执行（无法关闭）"
+		Me.RadioButton1.UseVisualStyleBackColor = True
+		'
+		'Label8
+		'
+		Me.Label8.AutoSize = True
+		Me.Label8.Location = New System.Drawing.Point(103, 185)
+		Me.Label8.Name = "Label8"
+		Me.Label8.Size = New System.Drawing.Size(275, 12)
+		Me.Label8.TabIndex = 15
+		Me.Label8.Text = "代码区（输出后自行复制到一个空的VBS文件里！）"
+		'
+		'Timer1
+		'
+		Me.Timer1.Interval = 1000
+		'
+		'Form1
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.BackColor = System.Drawing.SystemColors.Info
+		Me.ClientSize = New System.Drawing.Size(800, 419)
+		Me.Controls.Add(Me.Label8)
+		Me.Controls.Add(Me.GroupBox3)
+		Me.Controls.Add(Me.GroupBox2)
+		Me.Controls.Add(Me.GroupBox1)
+		Me.Controls.Add(Me.Label5)
+		Me.Controls.Add(Me.Label4)
+		Me.Controls.Add(Me.Label3)
+		Me.Controls.Add(Me.ProgressBar1)
+		Me.Controls.Add(Me.TextBox1)
+		Me.Controls.Add(Me.Label2)
+		Me.Controls.Add(Me.Button4)
+		Me.Controls.Add(Me.Button3)
+		Me.Controls.Add(Me.Button2)
+		Me.Controls.Add(Me.Button1)
+		Me.Controls.Add(Me.Label1)
+		Me.Controls.Add(Me.MenuStrip1)
+		Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+		Me.MainMenuStrip = Me.MenuStrip1
+		Me.MaximizeBox = False
+		Me.Name = "Form1"
+		Me.Text = "VBS Making"
+		Me.MenuStrip1.ResumeLayout(False)
+		Me.MenuStrip1.PerformLayout()
+		Me.GroupBox1.ResumeLayout(False)
+		Me.GroupBox1.PerformLayout()
+		Me.GroupBox2.ResumeLayout(False)
+		Me.GroupBox2.PerformLayout()
+		Me.GroupBox3.ResumeLayout(False)
+		Me.GroupBox3.PerformLayout()
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
+
+	End Sub
+
+	Friend WithEvents Label1 As Label
+	Friend WithEvents MenuStrip1 As MenuStrip
+	Friend WithEvents 关于ToolStripMenuItem As ToolStripMenuItem
+	Friend WithEvents 新项目ToolStripMenuItem As ToolStripMenuItem
+	Friend WithEvents Button1 As Button
+	Friend WithEvents Button2 As Button
+	Friend WithEvents Button3 As Button
+	Friend WithEvents Button4 As Button
+	Friend WithEvents Label2 As Label
+	Friend WithEvents TextBox1 As TextBox
+	Friend WithEvents ProgressBar1 As ProgressBar
+	Friend WithEvents Label3 As Label
+	Friend WithEvents Label4 As Label
+	Friend WithEvents Label5 As Label
+	Friend WithEvents GroupBox1 As GroupBox
+	Friend WithEvents TextBox3 As TextBox
+	Friend WithEvents Label7 As Label
+	Friend WithEvents TextBox2 As TextBox
+	Friend WithEvents Label6 As Label
+	Friend WithEvents GroupBox2 As GroupBox
+	Friend WithEvents CheckBox3 As CheckBox
+	Friend WithEvents CheckBox2 As CheckBox
+	Friend WithEvents CheckBox1 As CheckBox
+	Friend WithEvents GroupBox3 As GroupBox
+	Friend WithEvents RadioButton2 As RadioButton
+	Friend WithEvents RadioButton1 As RadioButton
+	Friend WithEvents Label8 As Label
+	Friend WithEvents Timer1 As Timer
+End Class
---
+++ Form1.vb
@@ -0,0 +1,53 @@
+﻿Public Class Form1
+	Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+		MsgBox("Hello! World", vbYesNo + vbInformation, "测试信息")
+	End Sub
+
+	Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+		Static Progress As Integer
+		Progress = 0
+		ProgressBar1.MarqueeAnimationSpeed = 2000
+		ProgressBar1.Value = Progress + 100
+		If ProgressBar1.Value = 100 Then
+			Label4.Text = 100
+			Timer1.Start()
+		End If
+	End Sub
+
+	Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+		RadioButton1.Checked = False 'reset
+		RadioButton2.Checked = False
+		CheckBox1.Checked = False
+		CheckBox2.Checked = False
+		CheckBox3.Checked = False
+		TextBox2.Text = ""
+		TextBox3.Text = ""
+	End Sub
+
+	Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+		TextBox1.Text = "" 'reset
+	End Sub
+
+	Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+		Label4.Text = "0"
+		ProgressBar1.MarqueeAnimationSpeed = 100
+		ProgressBar1.Value = 0
+		Timer1.Enabled = False
+	End Sub
+
+	Private Sub 关于ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles 关于ToolStripMenuItem.Click
+		MsgBox("BY Yeditie_
+Made By C#  2024.5.2
+ZCY11233833@163.com", vbYes + vbQuestion, "关于此程序")
+	End Sub
+
+	Private Sub 新项目ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles 新项目ToolStripMenuItem.Click
+		Do
+			MsgBox("对的，这是一个简单的病毒吗？", "666")
+		Loop
+	End Sub
+
+	Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+	End Sub
+End Class
